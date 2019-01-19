7e66d04dac2a7939970447ee135e5cedd0557412
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2019-01-19 19:49:39 +0100

    Small flavor cleanup

--- pot-images/bsd-ci-rust-nightly.sh
+++ pot-images/bsd-ci-rust-nightly.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-if [ "$(uname -r)" != "10.1-RELEASE" ]; then
-	[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
-	ASSUME_ALWAYS_YES=yes pkg bootstrap
-fi
+[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+ASSUME_ALWAYS_YES=yes pkg bootstrap
 touch /etc/rc.conf
 sysrc sendmail_enable="NONE"
 
 pkg install -y ca_root_nss curl
+
 fetch -o /root/rustup.sh https://sh.rustup.rs
 sh /root/rustup.sh -y --default-toolchain nightly
 export PATH="$HOME/.cargo/bin:$PATH"
+echo setenv PATH $HOME/.cargo/bin:'$PATH' >> $HOME/.cshrc
+
 rustup component add clippy-preview
 rustup component add rustfmt
-echo setenv PATH $HOME/.cargo/bin:'$PATH' >> $HOME/.cshrc
+
+pkg clean -aqy
