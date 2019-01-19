a94b2bed7ad0dd8b355d2547ead69d507a036ac5
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2019-01-19 17:58:16 +0100

    Add pot flavours to build images

---
+++ pot-images/bsd-ci-rust-stable.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$(uname -r)" != "10.1-RELEASE" ]; then
+	[ -w /etc/pkg/FreeBSD.conf ] && sed -i '' 's/quarterly/latest/' /etc/pkg/FreeBSD.conf
+	ASSUME_ALWAYS_YES=yes pkg bootstrap
+fi
+touch /etc/rc.conf
+sysrc sendmail_enable="NONE"
+
+pkg install -y ca_root_nss curl
+fetch -o /root/rustup.sh https://sh.rustup.rs
+sh /root/rustup.sh -y --default-toolchain stable
+export PATH="$HOME/.cargo/bin:$PATH"
+#echo PATH "$HOME/.cargo/bin:$PATH"
+rustup component add clippy-preview
+rustup component add rustfmt
+echo setenv PATH $HOME/.cargo/bin:'$PATH' >> $HOME/.cshrc
